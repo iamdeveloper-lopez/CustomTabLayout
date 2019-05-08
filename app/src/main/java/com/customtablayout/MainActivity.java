4a233c0806d713e70ac59c6722291a9cf08aa40d
Lester Lopez <lester@ldr.sg>
2019-05-08 18:48:01 +0800

    Update sample app shell

--- app/src/main/java/com/customtablayout/MainActivity.java
+++ app/src/main/java/com/customtablayout/MainActivity.java
@@ -1,7 +1,7 @@
 package com.customtablayout;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
 
     private CustomTabLayout tabLayout;
 
-    private View selectedTab;
+    private CustomTabLayout.Tab selectedTab;
+
+    private boolean hasFavorites = false;
+    private boolean hasItinerary = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +26,66 @@ public class MainActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tabLayout);
         tabLayout.addOnTabSelectedListener(new OnTabSelectedListener() {
             @Override
-            public void onTabSelected(CustomTabLayout.Tab tab) {
-                super.onTabSelected(tab);
-                selectedTab = tab;
+            public void onTabSelected(CustomTabLayout.Tab selectedTab) {
+                MainActivity.this.selectedTab = selectedTab;
+            }
+
+            @Override
+            public void onTabReselected(CustomTabLayout.Tab tab) {
+                if (hasFavorites) {
+                    hasItinerary = true;
+                }
+                if (!hasFavorites) {
+                    hasFavorites = true;
+                }
+                enableTabs(getEnableSize());
+            }
+
+            @Override
+            public void onTabUnselected(CustomTabLayout.Tab var1) {
+                for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                    if (i != tabLayout.getTabPosition(selectedTab)) {
+                        CustomTabLayout.Tab tab = tabLayout.getTabAt(i);
+                        if (tabLayout.getTabPosition(selectedTab) > i) {
+                            tab.setSelected(true);
+                        } else {
+                            tab.setSelected(false);
+                        }
+                    }
+                }
             }
         });
 
         tabLayout.setSelectedTabAt(0);
 
+        enableTabs(getEnableSize());
+
     }
+
+    private int getEnableSize() {
+        int size = 1;
+        if (hasFavorites) {
+            size++;
+        }
+        if (hasItinerary) {
+            size++;
+        }
+        return size;
+    }
+
+    private void enableTabs(int size) {
+        if (tabLayout.getTabCount() >= size) {
+            for (int i = 0; i < tabLayout.getTabCount(); i++) {
+                CustomTabLayout.Tab tab = tabLayout.getTabAt(i);
+                if ((i + 1) <= size) {
+                    tab.setEnabled(true);
+                    Log.d(TAG, "enableTabs: true " + tab.getText());
+                } else {
+                    tab.setEnabled(false);
+                    Log.d(TAG, "enableTabs: false " + tab.getText());
+                }
+            }
+        }
+    }
+
 }
